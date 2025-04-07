136e91dbb11ab087e784166c2ea9f0dd5d8aba2d
DXJ <137505064+frendicahyow@users.noreply.github.com>
2025-04-07 18:00:36 +0800

    Update bot.py

--- reshareshing/bot.py
+++ reshareshing/bot.py
@@ -63,10 +63,21 @@ SPREADSHEET_ID = load_spreadsheet_id()
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
 # Inisialisasi Google Sheets API
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'credentials.json'
+
+# Dapatkan direktori script saat ini (misal: DROPXJUNGLER/reshareshing)
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Dapatkan direktori induk, yaitu folder DROPXJUNGLER
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+
+# Susun path lengkap ke file credentials.json di folder induk
+credentials_path = os.path.join(parent_dir, 'credentials.json')
+
+# Muat kredensial dari file di folder induk
 creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    credentials_path, scopes=SCOPES)
 sheets_service = build('sheets', 'v4', credentials=creds)
 
 # ================================================================
