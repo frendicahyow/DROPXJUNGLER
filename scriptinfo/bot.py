d6207b9d0ad231503580efd1ec06bdf0c9236eab
DXJ <137505064+frendicahyow@users.noreply.github.com>
2025-04-07 18:04:45 +0800

    Update bot.py

--- scriptinfo/bot.py
+++ scriptinfo/bot.py
@@ -80,11 +80,22 @@ def send_to_telegram(message, thread_id=None):
 # GOOGLE SHEETS PENGIRIMAN
 ########################################
 def build_sheets_service():
-    credentials = service_account.Credentials.from_service_account_file(
-        'credentials.json',
-        scopes=['https://www.googleapis.com/auth/spreadsheets']
-    )
-    return build('sheets', 'v4', credentials=credentials)
+  # Inisialisasi Google Sheets API
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Dapatkan direktori script saat ini (misal: DROPXJUNGLER/reshareshing)
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Dapatkan direktori induk, yaitu folder DROPXJUNGLER
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+
+# Susun path lengkap ke file credentials.json di folder induk
+credentials_path = os.path.join(parent_dir, 'credentials.json')
+
+# Muat kredensial dari file di folder induk
+creds = service_account.Credentials.from_service_account_file(
+    credentials_path, scopes=SCOPES)
+sheets_service = build('sheets', 'v4', credentials=creds) 
 
 def send_to_sheets(record, sheet_name):
     """
